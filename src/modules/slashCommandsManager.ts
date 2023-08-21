68c0cbdf742af82f6f93552aa6e1c2a70daf9a99
HwaHyang <me@hwahyang.space>
2023-08-21 15:32:56 +0900

    feat: added slashCommandsManager
    work in progress

---
+++ src/modules/slashCommandsManager.ts
@@ -0,0 +1,320 @@
+'use strict';
+
+import {
+	GuildMember,
+	ChatInputCommandInteraction,
+	SlashCommandBuilder,
+	PermissionsBitField,
+	EmbedBuilder,
+	ButtonBuilder,
+	ActionRowBuilder,
+	ButtonStyle,
+} from 'discord.js';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v10';
+
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+import DataManager from './dataManager';
+
+import { IData, IGroup, IMenu, IParticipants, ISession } from '../template/IData';
+
+class SlashCommandsManager {
+	public readonly dateRule = /^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/;
+
+	constructor() {
+		dayjs.extend(customParseFormat);
+		dayjs.extend(utc);
+		dayjs.extend(timezone);
+		dayjs.tz.setDefault('Asia/Seoul');
+	}
+
+	public getSlashCommands(): Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'>[] {
+		return [
+			new SlashCommandBuilder()
+				.setName('모임추가')
+				.setDescription('새로운 모임 일정을 추가합니다. (서버 관리자 & 봇 관리자 전용)')
+				.addStringOption((option) =>
+					option
+						.setName('모임명')
+						.setDescription('모임의 이름을 지정합니다.')
+						.setRequired(true)
+				)
+				.addStringOption((option) =>
+					option
+						.setName('날짜')
+						.setDescription('모임의 날짜를 지정합니다. (YYYY-MM-DD)')
+						.setRequired(true)
+				)
+				.addIntegerOption((option) =>
+					option
+						.setName('인원')
+						.setDescription('최대 인원을 지정합니다.')
+						.setRequired(true)
+				),
+			new SlashCommandBuilder()
+				.setName('메뉴추가')
+				.setDescription('모임에 메뉴를 추가합니다. (서버 관리자 & 봇 관리자 전용)')
+				.addStringOption((option) =>
+					option
+						.setName('모임코드')
+						.setDescription('모임의 코드를 지정합니다.')
+						.setRequired(true)
+				)
+				.addStringOption((option) =>
+					option
+						.setName('가게명')
+						.setDescription('가게명을 지정합니다.')
+						.setRequired(true)
+				)
+				.addStringOption((option) =>
+					option
+						.setName('메뉴명')
+						.setDescription('메뉴명을 지정합니다.')
+						.setRequired(true)
+				)
+				.addIntegerOption((option) =>
+					option
+						.setName('가격')
+						.setDescription('메뉴의 가격을 지정합니다. (KRW)')
+						.setRequired(true)
+				),
+			new SlashCommandBuilder()
+				.setName('모임활성화')
+				.setDescription('모임의 활성화 여부를 지정합니다. (서버 관리자 & 봇 관리자 전용)')
+				.addStringOption((option) =>
+					option
+						.setName('모임코드')
+						.setDescription('모임의 코드를 지정합니다.')
+						.setRequired(true)
+				),
+			new SlashCommandBuilder()
+				.setName('모임목록')
+				.setDescription('모든 모임의 목록을 보여줍니다.')
+				.addStringOption((option) =>
+					option
+						.setName('유효성검증')
+						.setDescription('마감일이 지나지 않은 모임만 표시 할 지 결정합니다.')
+						.setRequired(false)
+				),
+		];
+	}
+
+	public async refreshSlashCommands(applicationId: string, token: string, guildId: string) {
+		const rest = new REST({ version: '9' }).setToken(token);
+		try {
+			await rest.put(Routes.applicationGuildCommands(applicationId, guildId), {
+				body: this.getSlashCommands(),
+			});
+
+			console.log('Successfully reloaded application (/) commands.');
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	public async processSlashCommand(
+		interaction: ChatInputCommandInteraction,
+		interactionMember: GuildMember,
+		botOwner: string
+	) {
+		let groupData: IGroup;
+		let groupId: string;
+
+		switch (interaction.commandName) {
+			case '모임추가':
+				// 권한 체크
+				if (
+					!interactionMember.permissions.has(PermissionsBitField.Flags.Administrator) &&
+					interactionMember.id !== botOwner
+				) {
+					await interaction.reply({
+						content:
+							'요청을 처리하지 못했습니다.\n`권한이 없습니다: Administrator(0x8) || Bot Owner(config/config.json)`',
+						ephemeral: true,
+					});
+					return;
+				}
+
+				const rawDate = interaction.options.getString('날짜');
+
+				// 날짜 정규식 체크
+				if (!this.dateRule.test(rawDate)) {
+					await interaction.reply({
+						content:
+							'요청을 처리하지 못했습니다.\n`날짜 형식이 올바르지 않습니다: YYYY-MM-DD (ex. 2023-08-21)`',
+						ephemeral: true,
+					});
+					return;
+				}
+
+				const date = dayjs(rawDate, 'YYYY-MM-DD');
+				const maxParticipants = interaction.options.getInteger('인원');
+				groupId = Math.random().toString(36).substring(2, 11);
+
+				let newGroupData = {
+					groupId: groupId,
+					holder: interaction.member.user.id,
+					name: interaction.options.getString('모임명'),
+					deadlineTimestamp: date.unix(),
+					maxParticipants: maxParticipants,
+				};
+
+				DataManager.getInstance().addGroupData(newGroupData);
+
+				interaction.reply({
+					content: `성공적으로 새로운 모임을 생성했습니다.\n모임의 코드는 \`${groupId}\`입니다.\n\n**※ 모임이 현재 비활성화 상태입니다. 참여자를 모집하려면, 반드시 모임을 활성화 상태로 변경 해 주세요.**`,
+					ephemeral: true,
+				});
+
+				break;
+			case '메뉴추가':
+				// 권한 체크
+				if (
+					!interactionMember.permissions.has(PermissionsBitField.Flags.Administrator) &&
+					interactionMember.id !== botOwner
+				) {
+					await interaction.reply({
+						content:
+							'요청을 처리하지 못했습니다.\n`권한이 없습니다: Administrator(0x8) || Bot Owner(config/config.json)`',
+						ephemeral: true,
+					});
+					return;
+				}
+
+				groupId = interaction.options.getString('모임코드');
+				if (!DataManager.getInstance().isGroupExist(groupId)) {
+					await interaction.reply({
+						content: `요청을 처리하지 못했습니다.\n\`유효하지 않은 모임코드 입니다: ${groupId}\``,
+						ephemeral: true,
+					});
+					return;
+				}
+				groupData = DataManager.getInstance().getGroupData(groupId);
+				const storeName = interaction.options.getString('가게명');
+
+				// 이미 등록되어 있는 정보가 있다면,
+				if (groupData.menus && groupData.menus.length > 0) {
+					// 동일한 가게가 존재하는지 확인
+					const index = groupData.menus.findIndex(
+						(target) => target.storeName == storeName
+					);
+					// 만약 동일한 가게가 존재한다면,
+					if (index >= 0) {
+						// 기존 정보에 추가
+						groupData.menus[index].menus.push(interaction.options.getString('메뉴명'));
+						groupData.menus[index].prices.push(interaction.options.getInteger('가격'));
+					}
+					// 동일한 가게가 존재하지 않는다면,
+					else {
+						// 새로운 가게 생성 + 메뉴 등록
+						const menuData: IMenu = {
+							storeName: storeName,
+							menus: [interaction.options.getString('메뉴명')],
+							prices: [interaction.options.getInteger('가격')],
+						};
+
+						groupData.menus.push(menuData);
+					}
+				}
+				// 등록되어 있는 정보가 없다면,
+				else {
+					// 새로운 객체 생성
+					groupData.menus = new Array<IMenu>();
+					// 새로운 가게 생성 + 메뉴 등록
+					const menuData: IMenu = {
+						storeName: storeName,
+						menus: [interaction.options.getString('메뉴명')],
+						prices: [interaction.options.getInteger('가격')],
+					};
+
+					groupData.menus.push(menuData);
+				}
+
+				DataManager.getInstance().deleteGroupData(groupId);
+
+				interaction.reply({
+					content: `성공적으로 아래의 메뉴를 등록했습니다.\n\`${storeName}\` - \`${interaction.options.getString(
+						'메뉴명'
+					)}\``,
+					ephemeral: true,
+				});
+
+				DataManager.getInstance().addGroupData(groupData);
+				break;
+			case '모임활성화':
+				// 권한 체크
+				if (
+					!interactionMember.permissions.has(PermissionsBitField.Flags.Administrator) &&
+					interactionMember.id !== botOwner
+				) {
+					await interaction.reply({
+						content:
+							'요청을 처리하지 못했습니다.\n`권한이 없습니다: Administrator(0x8) || Bot Owner(config/config.json)`',
+						ephemeral: true,
+					});
+					return;
+				}
+
+				groupId = interaction.options.getString('모임코드');
+				if (!DataManager.getInstance().isGroupExist(groupId)) {
+					await interaction.reply({
+						content: `요청을 처리하지 못했습니다.\n\`유효하지 않은 모임코드 입니다: ${groupId}\``,
+						ephemeral: true,
+					});
+					return;
+				}
+				groupData = DataManager.getInstance().getGroupData(groupId);
+
+				const embed = new EmbedBuilder()
+					.setTitle(`${groupData.name} 모임 개최 중!`)
+					.setDescription(`<@${groupData.holder}>님의 모임이 현재 개최 중입니다!`)
+					.setFields({
+						name: '모임 정보',
+						value: `- 모임명: \`${groupData.name}\`\n- 인원 제한: ${groupData.maxParticipants}명\n- 모임 마감: <t:${groupData.deadlineTimestamp}>`,
+						inline: false,
+					})
+					.setColor(0xbbf1ff)
+					.setFooter({ text: `모임 코드: ${groupData.groupId}` });
+
+				groupData.menus.forEach((target) => {
+					let values: string = '';
+					for (let i = 0; i < target.menus.length; i++) {
+						values += `- ${target.menus[i]} (${target.prices[i]
+							.toString()
+							.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원)\n`;
+					}
+					embed.addFields({ name: target.storeName, value: values, inline: false });
+				});
+
+				const buttons = new ActionRowBuilder<ButtonBuilder>();
+				buttons.addComponents(
+					new ButtonBuilder()
+						.setCustomId(`acceptGroup_${groupId}`)
+						.setLabel('모임 참여하기')
+						.setStyle(ButtonStyle.Primary)
+				);
+				buttons.addComponents(
+					new ButtonBuilder()
+						.setCustomId(`statusGroup_${groupId}`)
+						.setLabel('모임 현황')
+						.setStyle(ButtonStyle.Secondary)
+				);
+
+				interaction.reply({
+					content: '성공적으로 요청을 수행했습니다.',
+					ephemeral: true,
+				});
+
+				interaction.channel.send({ embeds: [embed], components: [buttons] });
+				break;
+			case '모임목록':
+				break;
+		}
+	}
+}
+
+export default SlashCommandsManager;
