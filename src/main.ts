f51617429340d649fa8b6b29e5920fafb6000a8d
HwaHyang <me@hwahyang.space>
2023-08-21 15:33:53 +0900

    feat: Custom modules integration into main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,62 @@
 'use strict';
 
-import * as path from 'path';
-import { Client, GatewayIntentBits } from 'discord.js';
+import {
+	Client,
+	GatewayIntentBits,
+	GuildMember,
+	ChatInputCommandInteraction,
+	ButtonInteraction,
+} from 'discord.js';
 
-const config = require(path.join(__dirname, '..', 'config', 'config.json'));
+import SlashCommandsManager from './modules/slashCommandsManager';
+import ButtonsManager from './modules/buttonsManager';
+import DataManager from './modules/dataManager';
+
+const config = require('../config/config.json');
 
 const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMessageReactions,
 	],
 });
+
+const slashCommandsManager = new SlashCommandsManager();
+const buttonsManager = new ButtonsManager();
 
 /////////////// Functions
 
 /////////////// Events
 client.on('ready', async () => {
 	console.log(`Logged in as ${client.user.tag}!`);
+	slashCommandsManager.refreshSlashCommands(client.application.id, config.token, config.guildId);
+});
+
+client.on('interactionCreate', async (rawInteraction) => {
+	// 지정되지 않은 Guild의 요청 무시
+	if (rawInteraction.guild.id !== config.guildId) return;
+
+	let interactionMember: GuildMember = rawInteraction.member as GuildMember;
+
+	// Slash Command Interaction
+	if (rawInteraction.isCommand()) {
+		let interaction: ChatInputCommandInteraction =
+			rawInteraction as ChatInputCommandInteraction;
+		slashCommandsManager.processSlashCommand(interaction, interactionMember, config.botOwner);
+	}
+	// Button Interaction
+	else if (rawInteraction.isButton()) {
+		let interaction: ButtonInteraction = rawInteraction as ButtonInteraction;
+		buttonsManager.processButton(interaction, interactionMember, config.botOwner);
+	}
 });
 
 /////////////// Entry
 (async () => {
-	client.login(config.Token);
+	client.login(config.token);
+
+	// constructor 호출
+	DataManager.getInstance();
 })();
